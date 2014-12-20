98810f1aa858ba916570ee86fc2af40eff388697
gubala <tg.errors@gmail.com>
2014-12-20 19:09:32 +0100

    new get district route and partial with styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users,
              controllers: {
-                omniauth_callbacks: "users/omniauth_callbacks"
+                omniauth_callbacks: 'users/omniauth_callbacks'
               }
 
   devise_scope :user do
     delete 'sign_out',
-        :to => 'devise/sessions#destroy',
-        :as => :destroy_user_session
+        to: 'devise/sessions#destroy',
+        as: :destroy_user_session
   end
 
-  scope "(:locale)", :locale => /pl|en/ do
+  scope '(:locale)', locale: /pl|en/ do
     root 'home#index'
+
+    resources :districts, only: [:show]
   end
 end
