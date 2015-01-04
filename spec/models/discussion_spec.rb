24ab48452b417f2d6c9c726412c732c6b5b328cb
gubala <tg.errors@gmail.com>
2015-01-04 16:00:20 +0100

    specs for recently_created discussions

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -14,4 +14,38 @@ RSpec.describe Discussion do
 
   end
 
+  describe '#recently_created' do
+
+    it 'returns empty array when no discussions' do
+      expect(Discussion.recently_created).to eq []
+      expect(Discussion.recently_created).not_to eq nil
+    end
+
+    it 'returns only 5 discussions' do
+      6.times { create(:discussion) }
+      expect(Discussion.recently_created.count).to eq 5
+    end
+
+    it 'returns all discussions when there are <=5 in total' do
+      3.times { create(:discussion) }
+      expect(Discussion.recently_created.count).to eq 3
+    end
+
+    it 'returns discussions in newest first order' do
+      create(:discussion, title: 'a')
+      create(:discussion, title: 'b', created_at: Time.now - 10.minutes)
+      create(:discussion, title: 'c', created_at: Time.now + 5.years)
+      result = Discussion.recently_created
+      expect(result.count).to eq 3
+      expect(result[0].title).to eq 'c'
+      expect(result[1].title).to eq 'a'
+      expect(result[2].title).to eq 'b'
+    end
+
+  end
+
+  describe '#recently_commented' do
+    pending 'Waiting for :recently_commented to be implemented.'
+  end
+
 end
