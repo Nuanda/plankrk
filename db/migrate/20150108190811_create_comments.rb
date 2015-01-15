4b071b658ac99ca806a37c2634ed09ecbe25eaf6
gubala <tg.errors@gmail.com>
2015-01-15 12:16:39 +0100

    db indices for created_at as we expect a lot of ordering on these fields

--- db/migrate/20150108190811_create_comments.rb
+++ db/migrate/20150108190811_create_comments.rb
@@ -11,5 +11,6 @@ class CreateComments < ActiveRecord::Migration
 
     add_foreign_key :comments, :users, column: :author_id
     add_foreign_key :comments, :discussions
+    add_index :comments, :created_at
   end
 end
